24feee106027cc5d3d17b0d57d2c96d5b679bdb9
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-15 14:55:00 -0400

    Mar-15-2024-2-54-59-PM

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
-
-
-<script setup>
-  import ImageGallery from './components/ImageGallery.vue';
-  import SideBar from './components/SideBar.vue'
-  import { ref } from 'vue';
-
-  const isWithColor = ref(true);
-
-  const onSelectOption = (isColor) => {
-    isWithColor.value = isColor;
-  } 
-</script>
-
 <template>
-  <v-app>
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    {{ isWithColor }}
-    yyyy
-    <SideBar @selectOption="onSelectOption" />
-    <v-main>
-      <ImageGallery :isWithColor="isWithColor"/>
-    </v-main>
-  </v-app>  
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link> |
+    <router-link to="/careers">Careers</router-link>
+  </nav>
+  <router-view/>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img width=200 alt="Vue logo" src="../assets/feature-gallery.svg">
+    <HelloWorld msg="Welcome to FeatureGallery"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
